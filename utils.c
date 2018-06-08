e06a8950272c3ee1dfee11b7b7dc0acd8617edf7
Panagiotis <papalotis1@gmail.com>
2018-06-09 01:09:54 +0200

    added functions that were in main

--- utils.c
+++ utils.c
@@ -13,6 +13,37 @@ void array_print(int *a, int size)
     printf("]\n");
 }
 
-float getTime() {
-    return (float)clock()/CLOCKS_PER_SEC;
+float getTime()
+{
+    return (float)clock() / CLOCKS_PER_SEC;
+}
+
+char **create_sudoku_string_array_from_file(char *filename, int num_sudokus)
+{
+
+    //allocate space for the sudoku strings
+    char **sud_str_array = (char **)malloc(num_sudokus * sizeof(char *));
+    for (int i = 0; i < num_sudokus; i++)
+    {
+        //allocate memory for every sudoku string
+        sud_str_array[i] = (char *)malloc(90 * sizeof(char));
+        memset(sud_str_array[i], 0, 90);
+    }
+
+    return get_sudokus_from_file(filename, sud_str_array, num_sudokus);
+}
+
+void free_sudoku_string_array(char **array, int size)
+{
+    //free the string array
+    for (int i = 0; i < size; i++)
+    {
+        free(array[i]);
+    }
+    free(array);
+}
+
+int strequals(char *s1, char *s2)
+{
+    return strcmp(s1, s2) == 0;
 }
--- utils.h
+++ utils.h
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include "file.h"
 
 #if !defined(UTILS_SUD)
 #define UTILS_SUD
 
-void array_print(int* a, int size);
+void array_print(int *a, int size);
 float getTime();
-
+char **create_sudoku_string_array_from_file(char *filename, int num_sudokus);
+void free_sudoku_string_array(char **array, int size);
+int strequals(char *s1, char *s2);
 
 #endif // UTILS_SUD
