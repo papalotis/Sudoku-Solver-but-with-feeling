bfa9af669fd2f807b65bd6d05432e69d47e64a88
Panagiotis <papalotis1@gmail.com>
2018-06-08 21:09:44 +0200

    run formmater and added a tostring function

--- stack.c
+++ stack.c
@@ -1,5 +1,6 @@
 #include "stack.h"
 #include <stdio.h>
+#include <string.h>
 
 #define STACK_EMPTY -1
 
@@ -15,9 +16,10 @@ that holds its next stack element
 /*
 This function creates an empty stack
 */
-stack* create_stack() {
+stack *create_stack()
+{
     //allocate the memory needed by the stack
-    stack* s = (stack*) malloc(sizeof(stack));
+    stack *s = (stack *)malloc(sizeof(stack));
     //set the sp to point at "nothing"
     s->sp = NULL;
     //the stack is empty so the size is 0
@@ -29,9 +31,10 @@ stack* create_stack() {
 /*
 This function creates an isolated element with a specific value
 */
-elem* create_elem(int n) {
+elem *create_elem(int n)
+{
     //allocate the memory needed by the element itself
-    elem* e = (elem*) malloc(sizeof(elem));
+    elem *e = (elem *)malloc(sizeof(elem));
     //set the data of the element the same as the given data
     e->data = n;
     //the element is isolated so it has no next element
@@ -45,7 +48,8 @@ This function frees a stack from memory. First it deletes/frees all the
 elements it the stack using the clear function and then frees the memory
 it uses itself
 */
-void free_stack(stack* s) {
+void free_stack(stack *s)
+{
     stack_clear(s);
     free(s);
 }
@@ -54,18 +58,20 @@ void free_stack(stack* s) {
 This function frees the memory that is used by a stack element
 Since it doesn't allocate any extra memory, it only needs to free itself
 */
-void free_elem(elem* e) {
+void free_elem(elem *e)
+{
     free(e);
 }
 
 /*
 This function pushes a value to the stack
 */
-void stack_push(stack* s, int n) {
+void stack_push(stack *s, int n)
+{
     //first get the current stack pointer
-    elem* next = s->sp;
+    elem *next = s->sp;
     //create the new element with the given value
-    elem* new = create_elem(n);
+    elem *new = create_elem(n);
     //set the stack pointer to point at the new element
     s->sp = new;
     //point the next of the new stack pointer to the previous one
@@ -77,19 +83,23 @@ void stack_push(stack* s, int n) {
 /*
 This function pushes many values to a stack
 */
-void stack_push_many(stack* s, int* data, int len) {
-    for (int i = 0; i < len; i++) {
+void stack_push_many(stack *s, int *data, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
         stack_push(s, data[i]);
     }
 }
 
-stack* stack_clone(stack* s) {
-    stack* copy = create_stack();
+stack *stack_clone(stack *s)
+{
+    stack *copy = create_stack();
     int vals[s->size];
 
-    elem* cur = s->sp;
+    elem *cur = s->sp;
     int counter = s->size - 1;
-    while(cur) {
+    while (cur)
+    {
         vals[counter--] = cur->data;
         cur = cur->next;
     }
@@ -100,9 +110,11 @@ stack* stack_clone(stack* s) {
 /*
 This function returns the value
 */
-int stack_peek(stack* s) {
+int stack_peek(stack *s)
+{
     //so long as the stack exists and the stack is not empty
-    if (s && !stack_is_empty(s)) {
+    if (s && !stack_is_empty(s))
+    {
         //return the value of the head
         return s->sp->data;
     }
@@ -114,14 +126,16 @@ int stack_peek(stack* s) {
 /*
 This function pops/removes the head of the stack and returns its value
 */
-int stack_pop(stack* s) {
+int stack_pop(stack *s)
+{
     //if the stack is empty an underflow would occur, return -1
-    if (stack_is_empty(s)) return STACK_EMPTY;
+    if (stack_is_empty(s))
+        return STACK_EMPTY;
 
     //get the value of the head
     int r = stack_peek(s);
     //store the current head
-    elem* prev = s->sp;
+    elem *prev = s->sp;
     //move the head to point to the next element
     s->sp = s->sp->next;
     //free the previous head
@@ -132,27 +146,31 @@ int stack_pop(stack* s) {
     return r;
 }
 
-
 /*
 This function removes the first element with a specific value
 */
-int stack_remove(stack* s, int val) {
+int stack_remove(stack *s, int val)
+{
     //holds the element we were looking at previously
-    elem* prev = NULL;
+    elem *prev = NULL;
     //holds the element that we are inspecting
     //(starting with the head of the stack)
-    elem* cur  = s->sp;
+    elem *cur = s->sp;
     //so long as there is an element to inspect
-    while (cur != NULL) {
+    while (cur != NULL)
+    {
         //if the element should be removed
-        if (cur->data == val) {
+        if (cur->data == val)
+        {
             //if we need to remove the head of the list
-            if (prev == NULL) {
+            if (prev == NULL)
+            {
                 //set the stack pointer to the next of the current
                 s->sp = cur->next;
             }
             //otherwise if it's another element
-            else {
+            else
+            {
                 //set the next of the previous element equal
                 //the next of the current element
                 prev->next = cur->next;
@@ -179,13 +197,15 @@ int stack_remove(stack* s, int val) {
 This function removes the all the elements with a specific value
 and returns how many elements were removed
 */
-int stack_remove_all(stack* s, int val) {
+int stack_remove_all(stack *s, int val)
+{
     //holds how many elements have been removed
     int count = 0;
     //holds the result of the remove_one function
     int rem = 1;
     //so long as we have removed another element
-    while(rem) {
+    while (rem)
+    {
         //try to remove another one
         rem = stack_remove(s, val);
         //increament the total count
@@ -198,64 +218,106 @@ int stack_remove_all(stack* s, int val) {
 /*
 This function deletes all the elements from the stack
 */
-void stack_clear(stack* s) {
+void stack_clear(stack *s)
+{
     //run this "forever"
-    while(1) {
+    while (1)
+    {
         //remove an element from the stack
         stack_pop(s);
         //if the stack is empty break out of the loop
-        if (stack_is_empty(s)) break;
+        if (stack_is_empty(s))
+            break;
     }
 }
 
 /*
 This function returns the size of the stack
 */
-int stack_get_size(stack* s) {
+int stack_get_size(stack *s)
+{
     return s->size;
 }
 
 /*
 A stack is empty when its size is 0
 */
-int stack_is_empty(stack* s) {
+int stack_is_empty(stack *s)
+{
     return stack_get_size(s) == 0;
+}
+
+char *stack_to_string(stack *s, char *buff)
+{
+    if (!s)
+        return NULL;
+
+    int str_len = 20 + 4 * stack_get_size(s);
+
+    //if the caller didn't provide a buffer to write
+    //we create one for ourselves.
+    //WARNING: This needs to be freed by someone but not us
+    if (buff == NULL)
+    {
+        buff = (char *)malloc(str_len * sizeof(char));
+    }
+
+    memset(buff, 0, str_len);
+
+    char num_str[3];
+    char *commaspace = ", ";
+    char *prefix = "Stack %p: [";
+    char *closingbracket = "]";
+
+    sprintf(buff, prefix, s);
+
+    elem *cur = s->sp;
+    //so long as the cur is a valid element
+    while (cur != NULL)
+    {
+        sprintf(num_str, "%d", cur->data);
+        strcat(buff, num_str);
+
+        //go the next element
+        cur = cur->next;
+        //if there is another element after the one that was printed
+        //print a comma for readability
+        if (cur != NULL)
+            strcat(buff, commaspace);
+    }
+
+    strcat(buff, closingbracket);
+
+    return buff;
 }
 
 /*
 This function prints the data of the stack to stdout
 */
-void stack_print(stack* s) {
-    //print a head
-    printf("Stack %p: [", s);
-    //start at the head of the stack
-    elem* cur = s->sp;
-    //so long as the cur is a valid element
-    while (cur != NULL) {
-        //print the data of the element
-        printf("%d", cur->data);
-        //go the next element
-        cur = cur->next;
-        //if there is another element after the one that was printed
-        //print a comma for readability
-        if (cur != NULL) printf(", ");
-    }
-    //closing bracket for readability
-    printf("]\n");
+void stack_print(stack *s)
+{
+    char *stack_str = stack_to_string(s, NULL);
+    //print the string
+    printf("%s\n", stack_str);
+    //free it from memory
+    free(stack_str);
 }
 
 /*
 This function returns true if there is an elemtent in the stack
 that has the specific value, otherwise false
 */
-int stack_contains(stack* s, int val) {
+int stack_contains(stack *s, int val)
+{
     //start with the top of the stack
-    elem* e = s->sp;
+    elem *e = s->sp;
     //so long as there are still elements to consider
-    while (e != NULL) {
+    while (e != NULL)
+    {
         //if that element has the same value as the one we are looking for
         //we can immediatelly return one
-        if (e->data == val) return 1;
+        if (e->data == val)
+            return 1;
         //otherwise move to the next element
         e = e->next;
     }
@@ -264,25 +326,30 @@ int stack_contains(stack* s, int val) {
     return 0;
 }
 
-/*
+/**
 This function returns true, if all the elements of two stacks are equal
 1-1.
 stack_equals([3,1,5], [3,1,5]) > true
 stack_equals([3,5,1], [3,1,5]) > false
 stack_equals([3], [3,5]) > false
 */
-int stack_equals(stack* s1, stack* s2) {
+int stack_equals(stack *s1, stack *s2)
+{
     //if two stacks are of different size then they are obviously not equal
-    if (stack_get_size(s1) != stack_get_size(s2)) return 0;
+    if (stack_get_size(s1) != stack_get_size(s2))
+        return 0;
 
     //declare two elem variables, that will each traverse each stack
-    elem* e1, *e2;
+    elem *e1, *e2;
     //they both start at the head of each stack
-    e1 = s1->sp; e2 = s2->sp;
+    e1 = s1->sp;
+    e2 = s2->sp;
     //so long as there are elements to consider
-    while (e1 != NULL) {
+    while (e1 != NULL)
+    {
         //if there are two elements that are not equal we can safely return false
-        if (e1->data != e2->data) return 0;
+        if (e1->data != e2->data)
+            return 0;
         //otherwise we need to consider the next elemtent of each stack
         e1 = e1->next;
         e2 = e2->next;
@@ -290,5 +357,4 @@ int stack_equals(stack* s1, stack* s2) {
     //we went through both stacks completely and could not find an unequal pair
     //therefore the stacks must be equal
     return 1;
-
 }
--- stack.h
+++ stack.h
@@ -4,35 +4,35 @@
 
 #include <stdlib.h>
 
-typedef struct _elem {
+typedef struct _elem
+{
     int data;
     struct _elem *next;
 } elem;
 
-typedef struct _stack {
-    elem* sp;
+typedef struct _stack
+{
+    elem *sp;
     int size;
 } stack;
 
-
-stack* create_stack();
-elem* create_elem(int n);
-void free_stack(stack* s);
-void free_elem(elem* e);
-void stack_push(stack* s, int n);
-void stack_push_many(stack* s, int* data, int len);
-stack* stack_clone(stack* s);
-int stack_peek(stack* s);
-int stack_pop(stack* s);
-int stack_remove(stack* s, int val);
-int stack_remove_all(stack* s, int val);
-void stack_clear(stack* s);
-int stack_get_size(stack* s);
-int stack_is_empty(stack* s);
-void stack_print(stack* s);
-int stack_contains(stack* s, int val);
-int stack_equals(stack* s1, stack* s2);
-
+stack *create_stack();
+elem *create_elem(int n);
+void free_stack(stack *s);
+void free_elem(elem *e);
+void stack_push(stack *s, int n);
+void stack_push_many(stack *s, int *data, int len);
+stack *stack_clone(stack *s);
+int stack_peek(stack *s);
+int stack_pop(stack *s);
+int stack_remove(stack *s, int val);
+int stack_remove_all(stack *s, int val);
+void stack_clear(stack *s);
+int stack_get_size(stack *s);
+int stack_is_empty(stack *s);
+char *stack_to_string(stack *s, char *buff);
+void stack_print(stack *s);
+int stack_contains(stack *s, int val);
+int stack_equals(stack *s1, stack *s2);
 
 #endif // STACK_H
-
